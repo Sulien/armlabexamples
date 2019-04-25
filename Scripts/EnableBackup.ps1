760d8511f4843a78ded369e2cef9826a19d6022e
vicky.esp <vicky.esp@gmail.com>
2019-04-25 14:22:50 +1000

    Commit

---
+++ Scripts/EnableBackup.ps1
@@ -0,0 +1,26 @@
+param (
+    [Parameter(Mandatory)]
+    [string]$vaultName = "vault728",
+
+    [Parameter(Mandatory)]
+    [string]$ResourceGroupName = "rg-backup",
+
+    [Parameter(Mandatory)]
+    [string]$VMResourceGroupName = "vdperdanadc",
+
+    [Parameter(Mandatory)]
+    [string]$VMResourceGroupName = "rg-addsdevenv"
+
+)
+
+$vault = Get-AzureRmRecoveryServicesVault -Name $vaultName -ResourceGroupName $resourceGroupName
+
+#Set Vault Context
+Set-AzureRmRecoveryServicesVaultContext -Vault $vault
+
+$policy = Get-AzureRmRecoveryServicesBackupProtectionPolicy -name "DailyPolicy"
+
+Enable-AzureRmRecoveryServicesBackupProtection `
+    -ResourceGroupName $VMResourceGroupName `
+    -Name $VMResourceGroupName `
+    -Policy $policy
